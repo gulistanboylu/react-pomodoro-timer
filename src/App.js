a3b2e848cda7bd683701554f3a5ab62673dfb3d2
Gulistan Boylu <gulistanboylu@Gulistans-MacBook-Pro.local>
2019-11-07 10:25:06 +0000

    Create button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import TomatoButton from './TomatoButton'
 
 function App () {
   return (
@@ -8,6 +9,9 @@ function App () {
         <p>
           Welcome to my awesome pomodoro timer! :D
         </p>
+        <TomatoButton name='start' onClick={this.startTimer.bind(this)} />
+        <TomatoButton name='pause' onClick={this.pauseTimer.bind(this)} />
+        <TomatoButton name='reset' onClick={this.stopTimer.bind(this)} />
       </header>
     </div>
   )
---
+++ src/TomatoButton.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class TomatoButton extends Component {
+  render () {
+    return <button>{this.props.name}</button>
+  }
+}
+
+export default TomatoButton
