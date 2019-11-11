64d16627a4b82d8e7ee8b9f9e2db5c3b5c49114d
Gulistan Boylu <gulistanboylu@Gulistans-MacBook-Pro.local>
2019-11-11 09:29:33 +0000

    Create timer on component mount

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import TomatoButton from './TomatoButton'
 
-function App () {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Welcome to my awesome pomodoro timer! :D
-        </p>
-        <TomatoButton name='start' onClick={this.startTimer.bind(this)} />
-        <TomatoButton name='pause' onClick={this.pauseTimer.bind(this)} />
-        <TomatoButton name='reset' onClick={this.stopTimer.bind(this)} />
-      </header>
-    </div>
-  )
+class App extends Component {
+  state = {
+    minutes: 25
+  }
+  handleStartTimer () {
+    console.log('timer is starting')
+  }
+
+  handlePauseTimer () {
+    console.log('pause timer')
+  }
+
+  handleStopTimer () {
+    console.log('stop timer')
+  }
+
+  componentDidMount () {
+    console.log('MOUNTED')
+    console.log(this.state.minutes)
+    function startTimer (duration, display) {
+      var timer = duration; var minutes; var seconds
+      setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10)
+
+        minutes = minutes < 10 ? '0' + minutes : minutes
+        seconds = seconds < 10 ? '0' + seconds : seconds
+
+        display.textContent = minutes + ':' + seconds
+
+        if (--timer < 0) {
+          timer = duration
+        }
+      }, 1000)
+    }
+
+    let minutes = this.state.minutes
+    window.onload = function () {
+     
+      var fiveMinutes = 60 * minutes
+      var display = document.querySelector('#time')
+      startTimer(fiveMinutes, display)
+    }
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <h1>testing testing</h1>
+        <header className='App-header'>
+          <p>
+           Welcome to my awesome pomodoro timer! :D
+          </p>
+          <div>Focus on task for <span id='time'>25:00</span> minutes!</div>
+          <div>Time left = <span id='timer' /></div>
+          <button name='start' onClick={this.handleStartTimer} />
+          <button name='pause' onClick={this.handlePauseTimer} />
+          <button name='reset' onClick={this.handleStopTimer} />
+        </header>
+      </div>)
+  }
 }
 
 export default App
