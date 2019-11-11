ea438ff1c895abd49a9e346a903615935f0cf033
Gulistan Boylu <gulistanboylu@Gulistans-MacBook-Pro.local>
2019-11-11 11:26:59 +0000

    Move set interval function under handle start timer function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
 import './App.css'
-import TomatoButton from './TomatoButton'
 
 class App extends Component {
-  state = {
-    minutes: 25
+  constructor (props) {
+    super(props)
+    this.state = {
+      minutes: 25
+    }
   }
 
   handleStartTimer () {
-    const fiveMinutes = 60 * this.state.minutes
+    const twentyFiveMinutes = 60 * this.state.minutes
     const display = document.querySelector('#time')
-    this.startTimer(fiveMinutes, display)
-  }
 
-  startTimer (duration, display) {
-    let timer = duration; let minutes; let seconds
+    let timer = twentyFiveMinutes; let minutes; let seconds
+
     setInterval(function () {
       minutes = parseInt(timer / 60, 10)
       seconds = parseInt(timer % 60, 10)
@@ -25,7 +25,7 @@ class App extends Component {
       display.textContent = minutes + ':' + seconds
 
       if (--timer < 0) {
-        timer = duration
+        timer = twentyFiveMinutes
       }
     }, 1000)
   }
@@ -39,6 +39,7 @@ class App extends Component {
   }
 
   componentDidMount () {
+
   }
 
   render () {
@@ -48,7 +49,7 @@ class App extends Component {
           <h1>Welcome to my awsome pomodoro time</h1>
         </header>
         <div>
-        <div>Focus on task for <span id='time'>25:00</span> minutes!</div>
+          <div>Focus on task for <span id='time'>25:00</span> minutes!</div>
           <button name='start' onClick={this.handleStartTimer.bind(this)}>Start</button>
           <button name='pause' onClick={this.handlePauseTimer.bind(this)}>Pause</button>
           <button name='reset' onClick={this.handleStopTimer.bind(this)}>Reset</button>
