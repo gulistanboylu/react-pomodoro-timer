e2ffc0c608b11ac95eeca9a1432c2262b6d83c66
Gulistan Boylu <gulistan.boylu@hindsightsoftware.co.uk>
2019-11-14 15:52:41 +0000

    WIP on pause button

--- src/App.js
+++ src/App.js
@@ -5,36 +5,67 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      minutes: 25
+      time: 60,
+      minutes: 60 * 25
+      // isPaused: false,
+      // display: '25.00'
     }
   }
 
   handleStartTimer () {
-    const twentyFiveMinutes = 60 * this.state.minutes
-    const display = document.querySelector('#time')
+    // this.timer = setInterval(() => this.setState({
+    //   time: this.state.time - 1
+    // }), 1000)
+    this.timer = setInterval(() => this.setState({
+      time: this.state.time - 1
+    }), 1000)
 
-    let timer = twentyFiveMinutes; let minutes; let seconds
+    // this.setState({
+    // minute = minute < 10 ? '0' + minute : minute,
+    // seconds = seconds < 10 ? '0' + seconds : seconds
+    // seconds = parseInt(timer % 60, 10)
+    // }), 1000)
+    // console.log(this.state.isPaused)
+    // if (this.state.isPaused === false) {
+    //   this.setState({
+    //     isPaused: true
+    //   })
+    // }
+    // if (this.state.isPaused === true) {
+    //   this.setState({
+    //     isPaused: false
+    //   })
+    // }
 
-    setInterval(function () {
-      minutes = parseInt(timer / 60, 10)
-      seconds = parseInt(timer % 60, 10)
+    // const twentyFiveMinutes = this.state.minutes
+    // const display = document.querySelector('#time')
 
-      minutes = minutes < 10 ? '0' + minutes : minutes
-      seconds = seconds < 10 ? '0' + seconds : seconds
+    // let timer = twentyFiveMinutes; let minutes; let seconds
 
-      display.textContent = minutes + ':' + seconds
+    // setInterval(function () {
+    //   // if (this.state.isPaused === true) {
+    //   minutes = parseInt(timer / 60, 10)
+    //   seconds = parseInt(timer % 60, 10)
 
-      if (--timer < 0) {
-        timer = twentyFiveMinutes
-      }
-    }, 1000)
-  }
+    //   minutes = minutes < 10 ? '0' + minutes : minutes
+    //   seconds = seconds < 10 ? '0' + seconds : seconds
 
-  handlePauseTimer () {
-    console.log('pause timer')
+    //   display.textContent = minutes + ':' + seconds
+
+    //   if (--timer < 0) {
+    //     timer = twentyFiveMinutes
+    //     // }
+    //   }
+    // }, 1000)
   }
 
   handleStopTimer () {
+    clearInterval(this.timer)
+    console.log('pause timer')
+  }
+
+  handleResetTimer () {
+    this.setState({ time: 60 })
     console.log('stop timer')
   }
 
@@ -42,17 +73,55 @@ class App extends Component {
 
   }
 
+  componentDidUpdate () {
+    console.log(this.state.time)
+    // if (this.state.isPaused === false) {
+    //   this.setState({
+    //     isPaused: true
+    //   })
+    // }
+    // if (this.state.isPaused === true) {
+    //   this.setState({
+    //     isPaused: false
+    //   })
+    // }
+
+    // const twentyFiveMinutes = 60 * this.state.minutes
+    // const display = document.querySelector('#time')
+
+    // let timer = twentyFiveMinutes; let minutes; let seconds
+
+    // setInterval(function () {
+    //   if (this.state.isPaused === true) {
+    //     minutes = parseInt(timer / 60, 10)
+    //     seconds = parseInt(timer % 60, 10)
+
+    //     minutes = minutes < 10 ? '0' + minutes : minutes
+    //     seconds = seconds < 10 ? '0' + seconds : seconds
+
+    //     display.textContent = minutes + ':' + seconds
+
+    //     if (--timer < 0) {
+    //       timer = twentyFiveMinutes
+    //     }
+    //   }
+    // }, 1000)
+  }
+
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
           <h1>Welcome to my awsome pomodoro time</h1>
+          <h2>{this.state.time}</h2>
         </header>
         <div>
-          <div>Focus on task for <span id='time'>25:00</span> minutes!</div>
-          <button name='start' onClick={this.handleStartTimer.bind(this)}>Start</button>
-          <button name='pause' onClick={this.handlePauseTimer.bind(this)}>Pause</button>
-          <button name='reset' onClick={this.handleStopTimer.bind(this)}>Reset</button>
+          {/* <div>Focus on task for <span id='time'>25:00</span> minutes!</div> */}
+          {/* <div>Focus on task for <span id='time'>{this.state.display}</span></div> */}
+          {/* <div>{this.state.minutes}</div> */}
+          <button name='start' onClick={this.handleStartTimer.bind(this)}>Start/Pause</button>
+          <button name='pause' onClick={this.handleStopTimer.bind(this)}>Pause</button>
+          <button name='reset' onClick={this.handleResetTimer.bind(this)}>Reset</button>
         </div>
       </div>)
   }
